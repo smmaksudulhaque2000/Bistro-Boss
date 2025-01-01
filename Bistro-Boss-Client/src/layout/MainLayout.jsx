201893ef2927cc2b468d07ebf1dd61db5bc96dbe
Maksudul_Haque <smmaksudulhaque2000@gmail.com>
2025-01-01 14:17:02 +0600

    SetUp Client Site

--- Bistro-Boss-Client/src/layout/MainLayout.jsx
+++ Bistro-Boss-Client/src/layout/MainLayout.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../page/shared/footer/Footer';
+import NavBar from '../page/shared/navbar/NavBar';
 
 const MainLayout = () => {
     return (
         <div>
-
+            <NavBar></NavBar>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
